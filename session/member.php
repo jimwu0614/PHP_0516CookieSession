7b33f5eef2aae5fe5c60f80bc1e618ade98e1bc6
chiachun wu <a0283137@gmail.com>
2022-05-16 11:20:44 +0800

    把所有既有的cookie換成session

---
+++ session/member.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>會員中心</title>
+</head>
+
+<body>
+    <?php session_start()?>
+    <h1>歡迎<?= $_SESSION['login']; ?></h1>
+    <li>
+        <a href="index.php">回首頁</a>
+    </li>
+    <li>
+        <a href="logout.php">登出</a>
+    </li>
+</body>
+
+</html>
