3f8a29bda05b4d7a169984fd909e01d253a9e112
chiachun wu <a0283137@gmail.com>
2022-05-16 10:16:49 +0800

    簡易cookie設定

---
+++ chklogin.php
@@ -0,0 +1,21 @@
+<?php
+$default_acc='123';
+$default_pw='321';
+
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+$error="";
+
+
+if ($acc!=$default_acc || $pw!=$default_pw) {
+    $error="帳號或密碼錯誤";
+    header("location:login.php?error=$error");    
+}else{
+    setcookie('login', $acc , time()+360);
+    header("location:member.php") ;
+}
+
+
+
+?>
